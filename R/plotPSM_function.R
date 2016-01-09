72d7ab6ad1ff507bc4c5b27e14467da0234be405
Karel L <krelopq@yahoo.es>
2016-01-09 18:56:53 +0100

    update plot

--- R/plotPSM_function.R
+++ R/plotPSM_function.R
@@ -2,7 +2,9 @@
 
 plot.psm <- function(psmObject, 
                      col = c("chartreuse4", "chartreuse3", "red3", "red4"), 
-                     names = c("Too cheap", "Cheap", "Expensive", "Too expensive")) {
+                     names = c("Too cheap", "Cheap", "Expensive", "Too expensive"),
+                     main=NULL, ylab=NULL, xlab="Price", 
+                     legend.position="top") {
 
   # Parada si la clase no es psm
   if (class(psmObject) != "psm")
@@ -40,48 +42,29 @@ plot.psm <- function(psmObject,
     } else {
       datadf <- rbind(datadf, tmp)
     }
-    # Se opera de forma similar con los puntos
-    puntos[i, 1] <- psmObject$results[[i]]$minprice[1]
-    puntos[i, 2] <- psmObject$results[[i]]$minprice[2]
-    puntos[i, 3] <- psmObject$results[[i]]$maxprice[1]
-    puntos[i, 4] <- psmObject$results[[i]]$maxprice[2]
-    puntos[i, 5] <- psmObject$results[[i]]$ipprice[1]
-    puntos[i, 6] <- psmObject$results[[i]]$ipprice[2]
-    puntos[i, 7] <- psmObject$results[[i]]$opprice[1]
-    puntos[i, 8] <- psmObject$results[[i]]$opprice[2]
-    names(puntos) <- c("xminprice", "yminprice", "xmaxprice", "ymaxprice",
-                       "xipprice", "yipprice", "xopprice", "yopprice")
 
   }
   
   levels(datadf$variable)<-names
+  datadf$ecdf<-datadf$ecdf*100
 
   # Si existe un solo grupo (por defecto) se utiliza la siguiente función
   # que añade los puntos de intersección en el gráfico
   if (psmObject$n.groups == 1) {
     xyplot(ecdf ~ price | group, group = variable, type = "l", data = datadf,
-           col = col_vector, lty = c(1, 2, 2, 1), lwd = 2, auto.key = TRUE, 
-           par.strip.text=list(col=c("white")),
-           strip=strip.custom(bg="black"),
+           col = col_vector, lty = c(1, 2, 2, 1), lwd = 2, auto.key = TRUE,
+           strip=FALSE,
            key = list(type = "l", text = list(label = levels(datadf$variable)),
                       lines = list(lty = c(1, 2, 2, 1), lwd = 2), 
                       column = nlevels(datadf$variable),
-                      space = "top", col = col_vector), xlab = list(label = "Price",
-                                                                    fontsize = 14), ylab = list(label = ""), scales = list(y = list(tck = c(1,
-                                                                                                                                            0)), x = list(tck = c(1, 0))), panel = function(x, y, ...) {
-                                                                                                                                              panel.xyplot(x, y, ...)
-                                                                                                                                              panel.points(puntos$xminprice, puntos$yminprice, col = "black",
-                                                                                                                                                           pch = 19)
-                                                                                                                                              panel.points(puntos$xmaxprice, puntos$ymaxprice, col = "black",
-                                                                                                                                                           pch = 19)
-                                                                                                                                              panel.points(puntos$xipprice, puntos$yipprice, col = "black",
-                                                                                                                                                           pch = 19)
-                                                                                                                                              panel.points(puntos$xopprice, puntos$yopprice, col = "black",
-                                                                                                                                                           pch = 19)
-                                                                                                                                            })
-
-    # En otro caso no se dibujan los puntos.
+                      space = legend.position, col = col_vector), 
+           main=main,
+           xlab = list(label = xlab, fontsize = 14), 
+           ylab = list(label = ylab), 
+           scales = list(y = list(tck = c(1, 0)), 
+                         x = list(tck = c(1, 0))))
   } else {
+    
     xyplot(ecdf ~ price | group, group = variable, type = "l", data = datadf,
            col = col_vector, lty = c(1, 2, 2, 1), lwd = 2, auto.key = TRUE,
            par.strip.text=list(col=c("white")),
@@ -89,8 +72,8 @@ plot.psm <- function(psmObject,
            key = list(type = "l", text = list(label = levels(datadf$variable)),
                       lines = list(lty = c(1, 2, 2, 1), lwd = 2),
                       column = nlevels(datadf$variable),
-                      space = "top", col = col_vector), xlab = list(label = "Price",
-                                                                    fontsize = 14), ylab = list(label = ""),  aspect = 0.5)
-
-  }
-}
+                      space = legend.position, col = col_vector), 
+           main=main,
+           xlab = list(label = xlab, fontsize = 14), 
+           ylab = list(label = ylab),  layout=c(1, psmObject$n.groups))
+}}
